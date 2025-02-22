d109bb30ebbb6650ec2fb9c38cc30f9c5f1fea73
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2025-02-22 11:17:03 +0600

    saving amount added

--- js/index.js
+++ js/index.js
@@ -6,16 +6,23 @@ document.getElementById('calculate').addEventListener('click', ()=>{
 
     const totalExpenses = software + courses + internet;
     const balance = income - totalExpenses;
-    // const totalExpensesElement = document.querySelector('#total-expenses');
-    // totalExpensesElement.innerText = totalExpenses
-   
     document.querySelector('#total-expenses').innerText = totalExpenses.toFixed(2);
     document.querySelector('#balance').innerText = balance.toFixed(2);
-
-    // document.querySelector('#balance').innerText = totalExpenses
-    
     document.querySelector('#results').classList.remove('hidden')
    
+})
+
+document.querySelector('#calculate-savings').addEventListener('click', ()=>{
+    const savingPercntace= parseFloat(document.querySelector('#savings').value);
+    const income = parseFloat(document.querySelector('#income').value);
+    const software = parseFloat(document.querySelector('#software').value);
+    const courses = parseFloat(document.querySelector('#courses').value);
+    const internet = parseFloat(document.querySelector('#internet').value);
+
+    const totalExpenses = software + courses + internet;
+    const balance = income - totalExpenses;
+    const savingAbout = (savingPercntace * balance) / 100;
+    document.querySelector('#savings-amount').innerText = savingAbout
     
     
     
